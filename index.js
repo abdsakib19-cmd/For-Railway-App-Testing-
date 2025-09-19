b7c2b852bce500ed58cb858ad0afd51e9ca95c4c
abdsakib19-cmd <abdsakib19@gmail.com>
2025-09-19 20:49:57 +0600

    initial project setup

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
+
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("🚀 Railway Test Project চলছে ঠিকঠাক!");
+  res.send("Hello from Railway 🚂");
 });
 
 app.listen(PORT, () => {
